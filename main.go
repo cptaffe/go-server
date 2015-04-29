5b39da7553b9b2913e6106d3a49e2fff45ff39b2
Connor Taffe <cpaynetaffe@gmail.com>
2015-04-28 23:22:55 -0500

    added dockerfile

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Test struct {
+	Message string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	test := Test{
+		fmt.Sprintf("Hi there, I love %s!", r.URL.Path[1:]),
+	}
+	b, err := json.Marshal(test)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "%s", b)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
